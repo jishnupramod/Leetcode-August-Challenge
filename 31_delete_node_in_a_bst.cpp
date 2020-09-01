a6961628ca553d498fdd6e671509cfc33fe5d13b
jishnupramod <jishnumagnanimous@gmail.com>
2020-09-01 22:08:19 +0530

    optimized solution

--- 31_delete_node_in_a_bst.cpp
+++ 31_delete_node_in_a_bst.cpp
@@ -162,3 +162,46 @@ public:
         return root;
     }
 };
+
+
+
+// Optimized version
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+        if (root->val < key)
+            root->right = deleteNode(root->right, key);
+        else if (root->val > key)
+            root->left = deleteNode(root->left, key);
+        else {
+            if (!root->left and !root->right)
+                root = nullptr;
+            else if (root->left) {
+                TreeNode* pred = root->left;
+                while (pred->right)
+                    pred = pred->right;
+                root->val = pred->val;
+                root->left = deleteNode(root->left, pred->val);
+            } else if (root->right) {
+                TreeNode* succ = root->right;
+                while (succ->left)
+                    succ = succ->left;
+                root->val = succ->val;
+                root->right = deleteNode(root->right, succ->val);
+            }
+        }
+        return root;
+    }
+};
